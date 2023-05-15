9845df71c80841148e9b67babf4822047adeca67
Inbell1s <50827900+Inbell1s@users.noreply.github.com>
2023-05-15 14:21:26 +0200

    Add multi job support

--- index.js
+++ index.js
@@ -6,61 +6,25 @@ const fs = require('fs')
 const ORACLE_ABI = require('./oracle.abi.json')
 const Web3Utils = require('web3-utils')
 
+const FAKE_RESPONSE = "0x000000000000000000000000000000000000000000000000000000c2797eab80"
+
 const {
-  ORACLE_ADDRESS: ENV_ORACLE_ADDRESS,
-  NODE_ADDRESS: ENV_NODE_ADDRESS,
-  FAKE_RESPONSE: ENV_FAKE_RESPONSE,
-  RPC_URL: ENV_RPC_URL,
-  BLOCK_INTERVAL: ENV_BLOCK_INTERVAL
-} = process.env
-
-// Parse command line arguments
-const args = process.argv.slice(2)
-let JOB_ID
-let START_BLOCK
-let END_BLOCK
-let ORACLE_ADDRESS = ENV_ORACLE_ADDRESS
-let NODE_ADDRESS = ENV_NODE_ADDRESS
-let FAKE_RESPONSE = ENV_FAKE_RESPONSE
-let RPC_URL = ENV_RPC_URL
-let BLOCK_INTERVAL = ENV_BLOCK_INTERVAL
-
-// Parse command line arguments
-for (let i = 0; i < args.length; i++) {
-  if (args[i] === '--job' && i + 1 < args.length) {
-    JOB_ID = args[i + 1]
-  } else if (args[i] === '--start' && i + 1 < args.length) {
-    START_BLOCK = args[i + 1]
-  } else if (args[i] === '--end' && i + 1 < args.length) {
-    END_BLOCK = args[i + 1]
-  } else if (args[i] === '--oracle' && i + 1 < args.length) {
-    ORACLE_ADDRESS = args[i + 1]
-  } else if (args[i] === '--node' && i + 1 < args.length) {
-    NODE_ADDRESS = args[i + 1]
-  } else if (args[i] === '--fake' && i + 1 < args.length) {
-    FAKE_RESPONSE = args[i + 1]
-  } else if (args[i] === '--rpc' && i + 1 < args.length) {
-    RPC_URL = args[i + 1]
-  } else if (args[i] === '--interval' && i + 1 < args.length) {
-    BLOCK_INTERVAL = args[i + 1]
-  }
-}
-
-const params = {
   ORACLE_ADDRESS,
   START_BLOCK,
-  JOB_ID,
   NODE_ADDRESS,
-  FAKE_RESPONSE,
   RPC_URL,
   BLOCK_INTERVAL
-}
-console.log('Running with', params)
+} = process.env
+
+let JOB_IDs = process.env.JOB_ID.split(',');
+JOB_IDs = JOB_IDs.map(id => id.replace(/-/g, ''));
+
+let END_BLOCK = process.env.END_BLOCK;
 
 const web3 = new Web3(RPC_URL)
 const oracle = new web3.eth.Contract(ORACLE_ABI, ORACLE_ADDRESS)
 
-async function getOracleRequestEvents(fromBlock, toBlock) {
+async function getOracleRequestEvents(fromBlock, toBlock, JOB_ID) {
   const body = {
     "jsonrpc": "2.0",
     "method": "eth_getLogs",
@@ -89,7 +53,7 @@ async function getOracleRequestEvents(fromBlock, toBlock) {
   const { result } = await response.json()
   const events = []
   result.forEach(event => {
-    const data = web3.eth.abi.decodeLog([{type:'bytes32',name:'jobId',indexed:true},{type:'address',name:'requester'},{type:'bytes32',name:'requestId',},{type:'uint256',name:'payment',},{type:'address',name:'callbackAddr',},{type:'bytes4',name:'callbackFunctionId',},{type:'uint256',name:'cancelExpiration',},{type:'uint256',name:'dataVersion',},{type:'bytes',name:'data',}],
+    const data = web3.eth.abi.decodeLog([{ type: 'bytes32', name: 'jobId', indexed: true }, { type: 'address', name: 'requester' }, { type: 'bytes32', name: 'requestId', }, { type: 'uint256', name: 'payment', }, { type: 'address', name: 'callbackAddr', }, { type: 'bytes4', name: 'callbackFunctionId', }, { type: 'uint256', name: 'cancelExpiration', }, { type: 'uint256', name: 'dataVersion', }, { type: 'bytes', name: 'data', }],
       event.data,
       event.topics);
     data.transactionHash = event.transactionHash
@@ -129,49 +93,52 @@ async function isFulfilledRequest(requestId) {
   return result === '0x0000000000000000000000000000000000000000000000000000000000000000'
 }
 
-async function main() {
+async function findRequests() {
   const step = Number(BLOCK_INTERVAL)
   let from
   let to
   const startBlock = Number(START_BLOCK || process.env.START_BLOCK)
-  const endBlock = END_BLOCK ? Number(END_BLOCK) : 9410087
+  let endBlock = END_BLOCK
+  if (endBlock === 'latest') {
+    endBlock = await web3.eth.getBlockNumber()
+  }
   const totalBlocks = endBlock - startBlock
-  let processedBlocks = 0
 
-  for (let i = startBlock; i < endBlock; i += step) {
-    from = Web3Utils.toHex(i)
-    to = '0x' + (Number(from) + step).toString(16)
-    console.log(`Start processing blocks from ${i} to ${Number(to)}`)
-    const requestEvents = await getOracleRequestEvents(from, to)
-    console.log(`We got ${requestEvents.length} request events. Start processing...`)
-
-    for (let requestEvent of requestEvents) {
-      const isFulfilled = await isFulfilledRequest(requestEvent.requestId)
-      if (!isFulfilled) {
-        console.log('Request without fulfillment found! Blocknumber is ' + Number(requestEvent.blockNumber))
-        const { requestId, payment, callbackAddr, callbackFunctionId, cancelExpiration } = requestEvent
-        const data = FAKE_RESPONSE
-        const tx = [requestId, payment, callbackAddr, callbackFunctionId, cancelExpiration, data]
-        const succesfullFulfill = await oracle.methods.fulfillOracleRequest(...tx).call({
-          from: NODE_ADDRESS
-        })
-        if (succesfullFulfill) {
-          await fs.appendFile('./storage/unfulfilled_requests', JSON.stringify(tx) + ',\n', () => {})
-        } else {
-          console.log('Something wrong with this request, we cannot fulfill it', requestEvent)
+  const jobs = JOB_IDs.map(async (JOB_ID) => {
+    let processedBlocks = 0
+    for (let i = startBlock; i < endBlock; i += step) {
+      from = Web3Utils.toHex(i)
+      to = '0x' + (Number(from) + step).toString(16)
+      const percentage = (processedBlocks / totalBlocks) * 100
+      console.log(`Processing blocks: ${i}-${Number(to)} for job ID: ${JOB_ID} | Progress: ${(i-START_BLOCK)}/${(endBlock-START_BLOCK)} (${percentage.toFixed(3)}%)`)
+      const requestEvents = await getOracleRequestEvents(from, to, JOB_ID)
+      if (requestEvents.length > 0) {
+        console.log(`We got ${requestEvents.length} request events. Start processing...`)
+        for (let requestEvent of requestEvents) {
+          const isFulfilled = await isFulfilledRequest(requestEvent.requestId)
+          if (!isFulfilled) {
+            console.log('Request without fulfillment found! Blocknumber is ' + Number(requestEvent.blockNumber))
+            const { requestId, payment, callbackAddr, callbackFunctionId, cancelExpiration } = requestEvent
+            const data = FAKE_RESPONSE
+            const tx = [requestId, payment, callbackAddr, callbackFunctionId, cancelExpiration, data]
+            const succesfullFulfill = await oracle.methods.fulfillOracleRequest(...tx).call({
+              from: NODE_ADDRESS
+            })
+            if (succesfullFulfill) {
+              await fs.appendFile(`./storage/unfulfilled_requests_${JOB_ID}`, JSON.stringify(tx) + ',\n', () => { })
+            } else {
+              console.log('Something wrong with this request, we cannot fulfill it', requestEvent)
+            }
+          }
+          // Save file specific to the jobId
+          await fs.writeFile(`./storage/${JOB_ID}`, to, () => { })
         }
       }
-
-      // Calculate and display percentage progress
       processedBlocks++
-      const percentage = (processedBlocks / totalBlocks) * 100
-      console.log(`Progress: ${percentage.toFixed(2)}%`)
-
-      // Save file specific to the jobId
-      await fs.writeFile(`./storage/${JOB_ID}`, to, () => {})
     }
-  }
+  });
+
+  await Promise.all(jobs);
 }
 
-main()
-
+findRequests();
